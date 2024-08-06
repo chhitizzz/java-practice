afb9e28c77125dfe391b9b29ffd34b716ea31872
Chhitiz Vaidya <chhitizvaidya@qniverse.co.uk>
2024-08-06 10:55:38 +0545

    first commit

---
+++ Exercise1.java
@@ -0,0 +1,7 @@
+// 1. Declare variables of different data types (byte, short, int, long, float, double, char, Boolean) and initialize them with values. Print these variables to the console.
+
+public class Exercise1 {
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
