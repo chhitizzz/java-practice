1bca7c9ab700c979c493817c8c13a48540ab229b
Chhitiz Vaidya <chhitizvaidya@qniverse.co.uk>
2024-08-06 13:15:55 +0545

    exercise 1

--- Exercise1.java
+++ Exercise1.java
@@ -2,6 +2,25 @@
 
 public class Exercise1 {
     public static void main(String[] args) {
-        System.out.println();
+
+        // Primitive Data Types
+        byte byteNumber = 124;
+        short shortNumber = 30000;
+        int intNumber = 123456;
+        long longNumber = 20000000000L;
+        float floatNumber = 5.67f;
+        double doubleNumber = 30.33d;
+        boolean boolValue = true;
+        char copyrightSymbol = '\u00A9';
+
+        System.out.println("This is a byte datatype: " + byteNumber);
+        System.out.println("This is a short datatype: " + shortNumber);
+        System.out.println("This is an int datatype: " + intNumber);
+        System.out.println("This is a long datatype: " + longNumber);
+        System.out.println("This is a float datatype: " + floatNumber);
+        System.out.println("This is a double datatype: " + doubleNumber);
+        System.out.println("This is a boolean datatype: " + boolValue);
+        System.out.println("This is a copyright symbol: " + copyrightSymbol);
+
     }
 }
