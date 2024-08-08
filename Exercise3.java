279ddc1568d6a5347eecb655b06d94da212694b9
Chhitiz Vaidya <chhitizvaidya@qniverse.co.uk>
2024-08-08 19:18:27 +0545

    exercise 3

--- Exercise3.java
+++ Exercise3.java
@@ -1,7 +1,28 @@
 // 3. Write a program that takes an integer input from the user and prints whether it is a prime number.
 
+import java.util.Scanner;
+
 public class Exercise3 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int num = sc.nextInt();
+
+        if(num > 1) {
+            for(int i =2; i < num; i++) {
+                if(num % i == 0) {
+                    System.out.println(num + " is not a prime number.");
+                    break;
+                }
+                else {
+                    System.out.println(num + " is a prime number.");
+                    break;
+                }
+            }
+        }
+        else {
+            System.out.println(num + " is not a prime number.");
+        }
 
     }
 }
