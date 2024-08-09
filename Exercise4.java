60a29a4b607aa03cccb7b2750e4316d9c4dd1c59
Chhitiz Vaidya <chhitizvaidya@qniverse.co.uk>
2024-08-09 10:33:56 +0545

    exercise 4

---
+++ Exercise4.java
@@ -0,0 +1,11 @@
+// 4. Write a program that takes a year as input from the user and checks if it is a leap year.
+
+import java.util.Scanner;
+
+public class Exercise4 {
+    public static void main(String[] args) {
+        Scanner sb = new Scanner(System.in);
+        System.out.println("Enter a year: ");
+        int year = sb.nextInt();
+    }
+}
