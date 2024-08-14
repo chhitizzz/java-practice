d34bdef3a9cb46b8f97aecd13a9239c1f37568f0
Chhitiz Vaidya <chhitizvaidya@qniverse.co.uk>
2024-08-14 13:21:25 +0545

    exercise 6

--- Exercise6.java
+++ Exercise6.java
@@ -1,7 +1,11 @@
 // 6. Write a program to print all the prime numbers between 1 and 100.
 
+import java.util.*;
+import java.util.stream.IntStream;
+
 public class Exercise6 {
     public static void main(String[] args) {
-
+        IntStream st = IntStream.range(1,101);
+        st.forEach(System.out::println);
     }
 }
