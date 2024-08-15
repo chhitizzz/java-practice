1d86f1d38e761e453fd1483c36aa22d2e4348f47
Chhitiz Vaidya <chhitizvaidya@qniverse.co.uk>
2024-08-15 11:08:43 +0545

    exercise 7

--- Exercise7.java
+++ Exercise7.java
@@ -1,7 +1,9 @@
 // 7. Write a program to reverse the elements of an array.
 
+import java.util.Arrays;
+
 public class Exercise7 {
     public static void main(String[] args) {
-
+        int[] intArray = {10, 20, 30, 40, 50};
     }
 }
