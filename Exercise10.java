1176518df5582642eb11bd9362e3c93cbe1c3528
Chhitiz Vaidya <chhitizvaidya@qniverse.co.uk>
2024-08-21 11:40:01 +0545

    exercise 10

--- Exercise10.java
+++ Exercise10.java
@@ -1,7 +1,22 @@
 // 10. Write a program that creates an 'ArrayList' of strings, adds some elements to it, and then sorts the elements in alphabetical order.
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Exercise10 {
     public static void main(String[] args) {
+        ArrayList <String> footballPlayers = new ArrayList<String>();
 
+        // Adding elements to the ArrayList
+        footballPlayers.add("Cristiano Ronaldo");
+        footballPlayers.add("Lionel Messi");
+        footballPlayers.add("Zinedine Zidane");
+        footballPlayers.add("Kylian Mbappe");
+        footballPlayers.add("Johan Cryuff");
+        footballPlayers.add("Gareth Bale");
+        footballPlayers.add("Wayne Rooney");
+        footballPlayers.add("Andres Iniesta");
+        footballPlayers.add("Yaya Toure");
+        footballPlayers.add("Iker Casillas")
     }
 }
