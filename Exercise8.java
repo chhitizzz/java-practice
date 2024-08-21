abab3f3b1bea9b3905b81fa01f99bcf6b49ce326
Chhitiz Vaidya <chhitizvaidya@qniverse.co.uk>
2024-08-21 10:59:38 +0545

    exercise 8

--- Exercise8.java
+++ Exercise8.java
@@ -1,7 +1,15 @@
 // 8. Write a program to find the second largest number in the array of integers.
 
-public class Exercise8 {
-    public static void main(String[] args) {
+import java.util.Arrays;
 
+public class Exercise8 {
+    public static int getSecondLargestInt(int[] num, int total) {
+        Arrays.sort(num);
+        return num[total - 2];
+    }
+
+    public static void main(String[] args) {
+        int num[] = {21, 98 ,52 ,4, 11, 10, 58, 76};
+        System.out.println("The second largest in the array is: " + getSecondLargestInt(num, 8));
     }
 }
