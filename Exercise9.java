7e19d98e33d3e997d577cfcfd79e6e3deee15e74
Chhitiz Vaidya <chhitizvaidya@qniverse.co.uk>
2024-08-21 11:18:11 +0545

    exercise 9

--- Exercise9.java
+++ Exercise9.java
@@ -1,7 +1,12 @@
 // 9. Write a program that takes a string input from the user and counts the number of vowels in the string.
 
+import java.util.*;
+
 public class Exercise9 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a word: ");
+        String str = sc.next();
 
     }
 }
