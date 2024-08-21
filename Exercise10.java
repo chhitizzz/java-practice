dfdc91860ba9f0bef80ea096b85bc38ae5c2c4bb
Chhitiz Vaidya <chhitizvaidya@qniverse.co.uk>
2024-08-21 11:31:13 +0545

    exercise 10

---
+++ Exercise10.java
@@ -0,0 +1,7 @@
+// 10. Write a program that creates an 'ArrayList' of strings, adds some elements to it, and then sorts the elements in alphabetical order.
+
+public class Exercise10 {
+    public static void main(String[] args) {
+
+    }
+}
