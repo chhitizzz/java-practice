3db7e36859605c201a3d40d27ccc8dd8c32cbafc
Chhitiz Vaidya <chhitizvaidya@qniverse.co.uk>
2024-08-23 15:43:45 +0545

    exercise 12

---
+++ Exercise12.java
@@ -0,0 +1,7 @@
+// 12. Write a program that uses a 'HashMap' to count the frequency of each character in a string.
+
+public class Exercise12 {
+    public static void main(String[] args) {
+
+    }
+}
